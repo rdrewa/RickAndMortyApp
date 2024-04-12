8c88a4e52ea748ec24ee0f0a3bd053421a33981b
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-12 11:14:38 +0200

    RMRA-9: Using notifier & showing data

--- lib/feature/home/presentation/screen/home_screen.dart
+++ lib/feature/home/presentation/screen/home_screen.dart
@@ -1,20 +1,39 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../../common/presentation/widget/app_drawer.dart';
+import '../widget/character_item.dart';
+import '../notifier/home_notifier.dart';
 
 @RoutePage()
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends HookConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    useMemoized(() async {
+      ref.read(homeNotifierProvider.notifier).getCharacterList();
+    });
+    final state = ref.watch(homeNotifierProvider);
+
     return Scaffold(
-      drawer: const AppDrawer(),
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Hello Rick&Morty World!'),
-      ),
-    );
+        appBar: AppBar(title: const Text('Characters')),
+        body: switch (state) {
+          HomeInitial() => const SizedBox.shrink(),
+          HomeLoading() => const CircularProgressIndicator(),
+          HomeLoaded() => ListView.builder(
+              itemCount: state.data.length,
+              itemBuilder: (context, index) =>
+                  CharacterItem(item: state.data[index])),
+          HomeFailure() => Center(child: Text(state.message)),
+        });
   }
 }
+
+
+/*
+Center(
+        child: Text('Hello, Rick X.Y-World!'),
+      )
+*/
