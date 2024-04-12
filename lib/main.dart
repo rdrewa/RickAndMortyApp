6700cc6fa32675ddb4806e7a8af264393a8ab282
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-12 11:07:28 +0200

    RMRA-9: Riverpod initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app/rick_morty_app.dart';
 import 'core/util/language_util.dart';
@@ -8,11 +9,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
-  runApp(EasyLocalization(
-      supportedLocales: LanguageUtil.supportedLocales,
-      path: 'assets/i18n',
-      fallbackLocale: LanguageUtil.locale,
-      startLocale: LanguageUtil.locale,
-      useOnlyLangCode: true,
-      child: const RickMortyApp()));
+  runApp(ProviderScope(
+    child: EasyLocalization(
+        supportedLocales: LanguageUtil.supportedLocales,
+        path: 'assets/i18n',
+        fallbackLocale: LanguageUtil.locale,
+        startLocale: LanguageUtil.locale,
+        useOnlyLangCode: true,
+        child: const RickMortyApp()),
+  ));
 }
