6550ebdceaab062fc7ba398bd9b10b21f8fe3bb5
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-14 21:46:43 +0200

    RMRA-11: Provider generated

--- lib/feature/details/domain/usecase/get_character_details_usecase.dart
+++ lib/feature/details/domain/usecase/get_character_details_usecase.dart
@@ -1,8 +1,16 @@
 import 'package:dartz/dartz.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '/core/error/failure.dart';
 import '../../../common/domain/repository/character_repository.dart';
 import '../model/character_details.dart';
+
+part 'get_character_details_usecase.g.dart';
+
+@riverpod
+GetCharacterDetailsUsecase getCharacterDetailsUsecase(
+        GetCharacterDetailsUsecaseRef ref) =>
+    GetCharacterDetailsUsecase(ref.watch(characterRepositoryProvider));
 
 class GetCharacterDetailsUsecase {
   final CharacterRepository _repository;
