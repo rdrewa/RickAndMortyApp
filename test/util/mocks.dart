94664fddbdbc1ac43ac23a0d4970e59912118169
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-15 00:24:00 +0200

    RMRA-13: Mocking RickGraphService

---
+++ test/util/mocks.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+
+import 'package:rick_morty_app/feature/common/data/source/remote/rick_service.dart';
+
+@GenerateNiceMocks([MockSpec<RickGraphService>()])
+void mocks() {}
