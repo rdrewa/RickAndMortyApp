5c2b1a7d88460afb8aa72ae5ab81b532acf846ff
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-15 23:21:59 +0200

    RMRA-19: FavoriteRepository (+Db) added

---
+++ lib/feature/common/data/repository/favorite_db_repository.dart
@@ -0,0 +1,63 @@
+import 'package:dartz/dartz.dart';
+import 'package:drift/native.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '/core/error/failure.dart';
+import '../../../home/domain/model/character_info.dart';
+import '../../domain/repository/favorite_repository.dart';
+import '../source/local/rick_database.dart';
+
+part 'favorite_db_repository.g.dart';
+
+@riverpod
+FavoriteDbRepository favoriteDbRepository(FavoriteDbRepositoryRef ref) =>
+    FavoriteDbRepository(ref.watch(rickDatabaseProvider));
+
+class FavoriteDbRepository implements FavoriteRepository {
+  final RickDatabase _database;
+
+  FavoriteDbRepository(this._database);
+
+  @override
+  Future<Either<Failure, void>> addItem(CharacterInfo item) async {
+    try {
+      await _database.insertFavorite(item.toFavorite());
+      return const Right(null);
+    } on SqliteException catch (s) {
+      return Left(DatabaseFailure(s.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<CharacterInfo>>> getList() async {
+    try {
+      final result = await _database.getFavoriteList();
+      final list = result
+          .map((favorite) => CharacterInfo.fromFavorite(favorite))
+          .toList();
+      return Right(list);
+    } on SqliteException catch (s) {
+      return Left(DatabaseFailure(s.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> isAdded(int id) async {
+    try {
+      final result = await _database.getFavorite(id);
+      return Right(result != null);
+    } on SqliteException catch (s) {
+      return Left(DatabaseFailure(s.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> removeItem(CharacterInfo item) async {
+    try {
+      await _database.deleteFavorite(int.parse(item.id));
+      return const Right(null);
+    } on SqliteException catch (s) {
+      return Left(DatabaseFailure(s.message));
+    }
+  }
+}
