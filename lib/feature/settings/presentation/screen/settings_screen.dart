6ccce2be21f2e4a733ce3c570b9905a7f8670f5f
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-16 23:40:21 +0200

    RMRA-23: SettingsScreen implemented

--- lib/feature/settings/presentation/screen/settings_screen.dart
+++ lib/feature/settings/presentation/screen/settings_screen.dart
@@ -1,13 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 import '/core/extension/build_context_theme_extension.dart';
-import '/core/util/language_util.dart';
 import '../../../common/presentation/widget/app_drawer.dart';
+import '../widget/language_settings.dart';
+import '../widget/theme_settings.dart';
 
 @RoutePage()
 class SettingsScreen extends HookConsumerWidget {
@@ -15,9 +15,6 @@ class SettingsScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isDarkTheme = useState<bool>(false);
-    final selectedLanguage = useState<String>('en');
-
     return Scaffold(
       appBar: AppBar(
         title: Text('settings.title'.tr()),
@@ -29,33 +26,7 @@ class SettingsScreen extends HookConsumerWidget {
             title: Text('settings.header'.tr(),
                 style: context.titleMedium
                     ?.copyWith(color: context.colorScheme.secondary)),
-            tiles: <SettingsTile>[
-              SettingsTile(
-                title: Text('settings.language'.tr()),
-                leading: const Icon(Icons.language),
-                trailing: DropdownButton<String>(
-                  value: selectedLanguage.value,
-                  onChanged: (newValue) {
-                    selectedLanguage.value = newValue!;
-                  },
-                  items: LanguageUtil.supportedLocales
-                      .map((locale) => DropdownMenuItem<String>(
-                            value: locale.languageCode,
-                            child: Text(
-                                'settings.langs.${locale.languageCode}'.tr(),
-                                style: TextStyle(
-                                    color: context.colorScheme.onBackground)),
-                          ))
-                      .toList(),
-                ),
-              ),
-              SettingsTile.switchTile(
-                onToggle: (value) => isDarkTheme.value = value,
-                initialValue: isDarkTheme.value,
-                leading: const Icon(Icons.dark_mode),
-                title: Text('settings.dark_theme'.tr()),
-              )
-            ],
+            tiles: <SettingsTile>[LanguageSettings(), ThemeSettings()],
           ),
         ],
       ),
