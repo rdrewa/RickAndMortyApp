b1fec7eef2f5dde786e5c243ba52de8cd25d22b3
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-17 05:06:34 +0200

    RMRA-25: Better DrawerHeader visibility

--- lib/feature/common/presentation/widget/app_drawer.dart
+++ lib/feature/common/presentation/widget/app_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:rick_morty_app/core/extension/build_context_theme_extension.dart';
 
 import '/app/app_router.dart';
 import 'drawer_tile.dart';
@@ -11,7 +12,14 @@ class AppDrawer extends StatelessWidget {
   Widget build(BuildContext context) => Drawer(
           child: ListView(
         children: [
-          DrawerHeader(child: Text('app.menu'.tr())),
+          DrawerHeader(
+              child: Center(
+            child: Text(
+              'app.menu'.tr(),
+              style: context.titleLarge
+                  ?.copyWith(color: context.colorScheme.tertiary),
+            ),
+          )),
           DrawerTile(
               title: 'home.title'.tr(),
               icon: Icons.home,
