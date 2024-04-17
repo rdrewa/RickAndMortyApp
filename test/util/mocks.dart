313a3a2db74feccae074889fd61ffb39f482de95
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-17 04:57:35 +0200

    RMRA-25: Mocking all repository, usecase & data source

--- test/util/mocks.dart
+++ test/util/mocks.dart
@@ -1,14 +1,28 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:mockito/annotations.dart';
+import 'package:rick_morty_app/feature/common/data/source/local/rick_database.dart';
 
 import 'package:rick_morty_app/feature/common/data/source/remote/rick_service.dart';
+import 'package:rick_morty_app/feature/common/domain/repository/character_repository.dart';
+import 'package:rick_morty_app/feature/common/domain/repository/favorite_repository.dart';
+import 'package:rick_morty_app/feature/details/domain/usecase/add_to_favorite_usecase.dart';
 import 'package:rick_morty_app/feature/details/domain/usecase/get_character_details_usecase.dart';
+import 'package:rick_morty_app/feature/details/domain/usecase/is_on_favorite_usecase.dart';
+import 'package:rick_morty_app/feature/details/domain/usecase/remove_from_favorite_usecase.dart';
+import 'package:rick_morty_app/feature/favorite/domain/usecase/get_favorite_list_usecase.dart';
 import 'package:rick_morty_app/feature/home/domain/usecase/get_character_list_usecase.dart';
 
 @GenerateNiceMocks([
   MockSpec<RickGraphService>(),
   MockSpec<GraphQLClient>(),
+  MockSpec<CharacterRepository>(),
+  MockSpec<FavoriteRepository>(),
+  MockSpec<RickDatabase>(),
   MockSpec<GetCharacterListUsecase>(),
-  MockSpec<GetCharacterDetailsUsecase>()
+  MockSpec<GetCharacterDetailsUsecase>(),
+  MockSpec<GetFavoriteListUsecase>(),
+  MockSpec<IsOnFavoriteUsecase>(),
+  MockSpec<AddToFavoriteUsecase>(),
+  MockSpec<RemoveFromFavoriteUsecase>(),
 ])
 void mocks() {}
