a1098bec1f02fa8df7f1813029e42f8ba8c20500
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-05-04 00:24:43 +0200

    RMRA-32: Proper interface

--- test/feature/home/domain/usecase/get_character_list_usecase_test.dart
+++ test/feature/home/domain/usecase/get_character_list_usecase_test.dart
@@ -2,13 +2,14 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
+import 'package:rick_morty_app/feature/common/domain/repository/character_repository.dart';
 import 'package:rick_morty_app/feature/home/domain/usecase/get_character_list_usecase.dart';
 
 import '../../../../util/data.dart';
 import '../../../../util/mocks.mocks.dart';
 
 void main() {
-  late final MockCharacterRepository mockCharacterRepository;
+  late final CharacterRepository mockCharacterRepository;
   late final GetCharacterListUsecase getCharacterListUsecase;
 
   setUp(() {
@@ -18,7 +19,7 @@ void main() {
 
   test('Should get Characters envelope from repository', () async {
     // arrange
-    when(mockCharacterRepository.getCharacterList(any))
+    when(mockCharacterRepository.getCharacterList(1))
         .thenAnswer((_) async => Right(testCharacters1));
 
     // act
