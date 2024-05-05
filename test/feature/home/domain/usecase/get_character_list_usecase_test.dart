7ea9d81fd571a0d29dfd8b76b372260e31f3ebaf
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-05-05 14:16:01 +0200

    RMRA-36: Broke test in order to launch cicd

--- test/feature/home/domain/usecase/get_character_list_usecase_test.dart
+++ test/feature/home/domain/usecase/get_character_list_usecase_test.dart
@@ -29,7 +29,7 @@ void main() {
     final result = await getCharacterListUsecase(1);
 
     // assert
-    expect(result, Right(testCharacters1));
+    expect(result, Right(testCharacterDetailsData1));
   });
 
   test('Should get Characters envelope empty from repository', () async {
