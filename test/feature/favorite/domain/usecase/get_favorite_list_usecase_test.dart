c4a533a3fee5fd1d6a7d780ead74c82e49b0692d
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-05-06 22:54:23 +0200

    RMRA-38: Left test for GetFavoriteListUsecase

--- test/feature/favorite/domain/usecase/get_favorite_list_usecase_test.dart
+++ test/feature/favorite/domain/usecase/get_favorite_list_usecase_test.dart
@@ -1,17 +1,19 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:rick_morty_app/core/error/failure.dart';
 
+import 'package:rick_morty_app/feature/common/domain/repository/favorite_repository.dart';
 import 'package:rick_morty_app/feature/favorite/domain/usecase/get_favorite_list_usecase.dart';
 
 import '../../../../util/data.dart';
 import '../../../../util/mocks.mocks.dart';
 
 void main() {
-  late final MockFavoriteRepository mockFavoriteRepository;
+  late final FavoriteRepository mockFavoriteRepository;
   late final GetFavoriteListUsecase getFavoriteListUsecase;
 
-  setUp(() {
+  setUpAll(() {
     mockFavoriteRepository = MockFavoriteRepository();
     getFavoriteListUsecase = GetFavoriteListUsecase(mockFavoriteRepository);
   });
@@ -26,5 +28,18 @@ void main() {
 
     // asseert
     expect(result, Right(testCharacterInfoList1));
+  });
+
+  test('Should return failure from repository', () async {
+    // arrange
+    const failureMessage = 'Database Failure';
+    when(mockFavoriteRepository.getList())
+        .thenAnswer((_) async => const Left(DatabaseFailure(failureMessage)));
+
+    // act
+    final result = await getFavoriteListUsecase();
+
+    // assert
+    expect(result, const Left(DatabaseFailure(failureMessage)));
   });
 }
