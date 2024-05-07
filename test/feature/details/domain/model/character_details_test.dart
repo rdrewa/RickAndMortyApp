5183d3ca949f4a686fc7e0794b4ed349988df5c8
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-05-07 19:10:55 +0200

    RMRA-40: Character details method toJson() tested

--- test/feature/details/domain/model/character_details_test.dart
+++ test/feature/details/domain/model/character_details_test.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rick_morty_app/feature/details/domain/model/character_details.dart';
+import 'package:rick_morty_app/feature/details/domain/model/episode.dart';
 
 import '../../../../util/data.dart';
 import '../../../../util/fixture.dart';
@@ -17,5 +18,21 @@ void main() {
 
     // assert
     expect(result, equals(testCharacterDetails1));
+  });
+
+  test('Should return a valid JSON map from object ', () async {
+    // arrange
+    final Map<String, dynamic> jsonMap =
+        json.decode(fixture('character_details'));
+
+    // act
+    final Map<String, dynamic> result = testCharacterDetails1.toJson();
+    result['origin'] = testCharacterDetails1.origin.toJson();
+    result['episode'] =
+        testCharacterDetails1.episode.map((Episode e) => e.toJson()).toList();
+    result['location'] = testCharacterDetails1.location.toJson();
+
+    // assert
+    expect(result, equals(jsonMap));
   });
 }
