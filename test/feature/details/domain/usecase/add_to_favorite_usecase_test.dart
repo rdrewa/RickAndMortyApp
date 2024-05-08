092a83f8610a2fa247f0a5e568e65d62e53a8a2c
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-05-08 23:08:08 +0200

    RMRA-40: Failure test

--- test/feature/details/domain/usecase/add_to_favorite_usecase_test.dart
+++ test/feature/details/domain/usecase/add_to_favorite_usecase_test.dart
@@ -2,23 +2,25 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
+import 'package:rick_morty_app/core/error/failure.dart';
+import 'package:rick_morty_app/feature/common/domain/repository/favorite_repository.dart';
 import 'package:rick_morty_app/feature/details/domain/usecase/add_to_favorite_usecase.dart';
 
 import '../../../../util/data.dart';
 import '../../../../util/mocks.mocks.dart';
 
 void main() {
-  late final MockFavoriteRepository mockFavoriteRepository;
+  late final FavoriteRepository mockFavoriteRepository;
   late final AddToFavoriteUsecase addToFavoriteUsecase;
 
-  setUp(() {
+  setUpAll(() {
     mockFavoriteRepository = MockFavoriteRepository();
     addToFavoriteUsecase = AddToFavoriteUsecase(mockFavoriteRepository);
   });
 
   test('Should return void/null after adding item to favorite', () async {
     // arrange
-    when(mockFavoriteRepository.addItem(any))
+    when(mockFavoriteRepository.addItem(testCharacterInfo1))
         .thenAnswer((_) async => const Right(null));
 
     // act
@@ -26,5 +28,18 @@ void main() {
 
     // asseert
     expect(result, const Right(null));
+  });
+
+  test('Should return failure after adding item to favorite', () async {
+    // arrange
+    const errorMessage = 'Database Failure';
+    when(mockFavoriteRepository.addItem(testCharacterInfo1))
+        .thenAnswer((_) async => const Left(DatabaseFailure(errorMessage)));
+
+    // act
+    final result = await addToFavoriteUsecase(testCharacterInfo1);
+
+    // assert
+    expect(result, const Left(DatabaseFailure(errorMessage)));
   });
 }
