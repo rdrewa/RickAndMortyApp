2b2432b0d38aa568e28524621ebeb2a07cbafa99
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-05-14 00:44:32 +0200

    RMRA-40: Fialure test added

--- test/feature/details/domain/usecase/remove_from_favorite_usecase_test.dart
+++ test/feature/details/domain/usecase/remove_from_favorite_usecase_test.dart
@@ -2,16 +2,18 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
+import 'package:rick_morty_app/core/error/failure.dart';
+import 'package:rick_morty_app/feature/common/domain/repository/favorite_repository.dart';
 import 'package:rick_morty_app/feature/details/domain/usecase/remove_from_favorite_usecase.dart';
 
 import '../../../../util/data.dart';
 import '../../../../util/mocks.mocks.dart';
 
 void main() {
-  late final MockFavoriteRepository mockFavoriteRepository;
+  late final FavoriteRepository mockFavoriteRepository;
   late final RemoveFromFavoriteUsecase removeFromFavoriteUsecase;
 
-  setUp(() {
+  setUpAll(() {
     mockFavoriteRepository = MockFavoriteRepository();
     removeFromFavoriteUsecase =
         RemoveFromFavoriteUsecase(mockFavoriteRepository);
@@ -19,7 +21,7 @@ void main() {
 
   test('Should return void/null after removing item from favorite', () async {
     // arrange
-    when(mockFavoriteRepository.removeItem(any))
+    when(mockFavoriteRepository.removeItem(testCharacterInfo1))
         .thenAnswer((_) async => const Right(null));
 
     // act
@@ -27,5 +29,18 @@ void main() {
 
     // asseert
     expect(result, const Right(null));
+  });
+
+  test('Should return failure after removing item from favorite', () async {
+    // arrange
+    const errorMessage = 'Database Failure';
+    when(mockFavoriteRepository.removeItem(testCharacterInfo1))
+        .thenAnswer((_) async => const Left(DatabaseFailure(errorMessage)));
+
+    // act
+    final result = await removeFromFavoriteUsecase(testCharacterInfo1);
+
+    // asseert
+    expect(result, const Left(DatabaseFailure(errorMessage)));
   });
 }
