d65f1a5a3976ee3690f62d47f6996747ac76bd34
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-05-14 00:59:57 +0200

    RMRA-40: Method toJson() tested

--- test/feature/details/data/model/character_details_data_test.dart
+++ test/feature/details/data/model/character_details_data_test.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rick_morty_app/feature/details/data/model/character_details_data.dart';
+import 'package:rick_morty_app/feature/details/domain/model/character_details.dart';
+import 'package:rick_morty_app/feature/details/domain/model/episode.dart';
 
 import '../../../../util/data.dart';
 import '../../../../util/fixture.dart';
@@ -17,5 +19,25 @@ void main() {
 
     // assert
     expect(result, equals(testCharacterDetailsData1));
+  });
+
+  test('Should return a valid CharacterDetailsData JSON map from object ',
+      () async {
+    // arrange
+    final Map<String, dynamic> jsonMap =
+        json.decode(fixture('character_details_data'));
+
+    // act
+    final Map<String, dynamic> result = testCharacterDetailsData1.toJson();
+    final CharacterDetails characterDetails =
+        testCharacterDetailsData1.characterDetails;
+    result['character'] = characterDetails.toJson();
+    result['character']['origin'] = characterDetails.origin.toJson();
+    result['character']['episode'] =
+        characterDetails.episode.map((Episode e) => e.toJson()).toList();
+    result['character']['location'] = characterDetails.location.toJson();
+
+    // assert
+    expect(result, equals(jsonMap));
   });
 }
